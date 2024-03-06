cfff5f6d6a853c71e7306ffe55d9a7bc3c2cebe2
Arey125 <andrej.kurenckoff@gmail.com>
2024-03-06 23:02:56 +0300

    feat: solved

--- kata/kata.go
+++ kata/kata.go
@@ -1,5 +1,27 @@
 package kata
 
-func Sum(a, b int) int {
-  return a + b
+func ValidISBN10(isbn string) bool {
+	if len(isbn) != 10 {
+		return false
+	}
+
+	sum := 0
+
+	for ind, c := range isbn {
+		if c == 'X' || c == 'x' {
+      if ind != 9 {
+        return false
+      }
+
+			sum += 10 * (ind + 1)
+			continue
+		}
+
+    if c < '0' || c > '9' {
+      return false
+    }
+		sum += int(c-'0') * (ind + 1)
+	}
+
+	return sum%11 == 0
 }
