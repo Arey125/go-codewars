9e3d06dd3eba53ea0498f8ede4fd4d96fd17e314
Arey125 <andrej.kurenckoff@gmail.com>
2024-03-07 21:29:43 +0300

    feat: completed

--- kata/kata.go
+++ kata/kata.go
@@ -1,6 +1,34 @@
 package kata
 
+func generate(generated map[string]bool, str []byte, l int) map[string]bool {
+	if l == 1 {
+    generated[string(str)] = true
+		return generated
+	}
+
+	generated = generate(generated, str, l-1)
+
+	for ind := range str[:l] {
+		if str[ind] == str[l-1] {
+			continue
+		}
+		str[ind], str[l-1] = str[l-1], str[ind]
+		generated = generate(generated, str, l-1)
+		str[ind], str[l-1] = str[l-1], str[ind]
+	}
+
+	return generated
+}
 
 func Permutations(s string) []string {
-  return make([]string, 0)
+	str := []byte(s)
+
+  resmap := generate(make(map[string]bool, 0), str, len(str))
+
+  res := make([]string, 0, len(resmap))
+  for k := range resmap {
+    res = append(res, k)
+  }
+
+  return res
 }
