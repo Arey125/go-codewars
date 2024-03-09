bbf9376bed07d4f37910657685bc3e15b0a0382f
Arey125 <andrej.kurenckoff@gmail.com>
2024-03-09 19:32:48 +0300

    feat: completed

--- kata/kata.go
+++ kata/kata.go
@@ -1,5 +1,16 @@
 package kata
 
-func Sum(a, b int) int {
-  return a + b
+func JosephusSurvivor(n, k int) int {
+    survivors := make([]int, n)
+    for ind := range survivors {
+        survivors[ind] = ind + 1
+    }
+
+    pos := 0
+    for len(survivors) != 1 {
+        pos = (pos + k - 1) % len(survivors)
+        survivors = append(survivors[:pos], survivors[pos + 1:]...)
+    }
+
+    return survivors[0]
 }
--- test.go
+++ test.go
@@ -6,6 +6,15 @@ import (
 )
 
 func main() {
-  fmt.Println(Sum(2, 3))
-  fmt.Println(5)
+	dotest(7, 3, 4)
+	dotest(11, 19, 10)
+	dotest(40, 3, 28)
+	dotest(14, 2, 13)
+	dotest(100, 1, 100)
+}
+
+func dotest(n, k, e int) {
+	fmt.Println(JosephusSurvivor(n, k))
+	fmt.Println(e)
+	fmt.Println()
 }
