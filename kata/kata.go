ea72b7ea5a8a1e94bf178a9f1f2a27ff2001f614
Arey125 <andrej.kurenckoff@gmail.com>
2024-03-17 20:52:25 +0300

    feat: completed

--- kata/kata.go
+++ kata/kata.go
@@ -1,5 +1,45 @@
 package kata
 
-func Sum(a, b int) int {
-  return a + b
+import "fmt"
+
+func Solution(list []int) string {
+	res := make([]byte, 0)
+
+	for ind, val := range list {
+		if ind == 0 {
+			res = append(res, []byte(fmt.Sprint(val))...)
+			continue
+		}
+
+		if ind == len(list)-1 {
+			if res[len(res)-1] != '-' {
+				res = append(res, ',')
+			}
+
+			res = append(res, []byte(fmt.Sprint(val))...)
+			continue
+		}
+
+		if val-1 == list[ind-1] && val+1 == list[ind+1] {
+			if res[len(res)-1] != '-' {
+				res = append(res, '-')
+			}
+		}
+
+		if val-1 != list[ind-1] {
+			res = append(res, byte(','))
+			res = append(res, []byte(fmt.Sprint(val))...)
+			continue
+		}
+
+		if val+1 != list[ind+1] {
+			if res[len(res)-1] != '-' {
+				res = append(res, ',')
+			}
+			res = append(res, []byte(fmt.Sprint(val))...)
+			continue
+		}
+	}
+
+	return string(res)
 }
--- test.go
+++ test.go
@@ -6,6 +6,11 @@ import (
 )
 
 func main() {
-  fmt.Println(Sum(2, 3))
-  fmt.Println(5)
+	s := Solution([]int{-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20})
+	fmt.Println(s)
+    fmt.Println("-6,-3-1,3-5,7-11,14,15,17-20")
+	fmt.Println()
+
+    fmt.Println(Solution([]int{1, 2, 4, 5, 7, 8}))
+	fmt.Println("1,2,4,5,7,8")
 }
